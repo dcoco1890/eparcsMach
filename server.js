109551c37c40350b727b190bbdb94fc61fa93f4c
Cohen <dcoco91@gmail.com>
2019-09-19 18:38:10 -0400

    basic server stuff

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-var express = require("express");
+let express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', function(req, res) {
+    res.send('hello world')
+})
+app.listen(PORT, () => {
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+    );
+});
