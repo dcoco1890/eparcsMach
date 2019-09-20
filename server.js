7d8a2afd82676b9168ca3993bf7bd90540ba546c
Cohen <dcoco91@gmail.com>
2019-09-20 13:40:33 -0400

    getting everything ready, added some server code to handle errors, serve handlebars, etc

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 let express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+let exphbs = require("express-handlebars");
 
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-const app = express();
 const PORT = process.env.PORT || 8080;
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+
+
+const app = express();
+
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false  }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
 app.get('/', function(req, res) {
-    res.send('hello world')
+    res.render("index");
 })
 app.listen(PORT, () => {
     console.log(
