2f8d78786dd6995b6cc47bef21fad242a28c19a8
Cohen <dcoco91@gmail.com>
2019-09-23 14:01:09 -0400

    added / changed routes around. content is now displayed on the screen

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,30 @@ var cheerio = require("cheerio");
 
 module.exports = app => {
 
-    app.get('/', function(req, res) {
-        res.render("index");
-    });
+    app.get("/scrape", (req, res) => {
+        axios.get("https://www.nbc12.com/news/richmond/").then( response => {
+            var $ = cheerio.load(response.data);
+           
+            $("a.unstyled-link").each( (i, element) => {
+                
+                let title = "https://www.nbc12.com/";
+                title += $(element).attr("href");
+                var resluts = {};
+                resluts.link = title;
+
+                db.Article.create(resluts).then(function(art){
+                    console.log(art);
+                }).catch(function(err){
+                    console.log(err);
+                });
+                
+            });
+            
+
+           console.log(resluts);
+
+        });
+        res.send("sca");
+
+    })
 }
--- routes/html.js
+++ routes/html.js
@@ -4,33 +4,16 @@ const cheerio = require("cheerio");
 
 module.exports = (app) => {
     app.get("/", (req, res) => {
-        res.render("index");
+        db.Article.find({}).sort("-id").limit(10).then(data => {
+            res.render("index" ,{
+                Article: data
+            });
+        }).catch(err => console.log(err));
+       
+        
     });
 
     app.get("/scrape", (req, res) => {
-        axios.get("https://www.nbc12.com/news/richmond/").then( response => {
-            var $ = cheerio.load(response.data);
-           
-            $("a.unstyled-link").each( (i, element) => {
-                
-                let title = "https://www.nbc12.com/";
-                title += $(element).attr("href");
-                var resluts = {};
-                resluts.link = title;
-
-                db.Article.create(resluts).then(function(art){
-                    console.log(art);
-                }).catch(function(err){
-                    console.log(err);
-                });
-                
-            });
-            
-
-           console.log(resluts);
-
-        });
-        res.send("sca");
-
+        res.render("scrape");
     })
 }
