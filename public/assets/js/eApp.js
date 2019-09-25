1e2e7d4564d925cfa0b5ca16bd288b9b4b2900e1
Cohen <dcoco91@gmail.com>
2019-09-24 22:47:33 -0400

    add text on click only one time

--- public/assets/js/eApp.js
+++ public/assets/js/eApp.js
@@ -5,19 +5,26 @@ $(document).ready(function() {
 
     box.on("click", function() {
         var id = $(this).attr("data-id");
-        $(this).toggleClass("boxOpen");
+        var at = $(this).attr("id");
+        if(at === "ran"){
+           
+        } else{
+            $(this).attr("id", "ran");
+            $.ajax({
+                type: "GET",
+                url: "/scrape/" + id
+            }).then(response => {
+                console.log(response);
+                console.log($(this));
+                var article = `<p class="text-info">${response.toString()}</p>`;
+                $(this).find(".art-bod").append(article);
+            }).catch(err => {
+                console.log(err);
+            });
+        }
+        // $(this).toggleClass("boxOpen");
 
-        $.ajax({
-            type: "GET",
-            url: "/scrape/" + id
-        }).then(response => {
-            console.log(response);
-            console.log($(this));
-            var article = `<div class="text-center"><p class="text-info">${response.toString()}</p></div>`;
-            $(this).append(article);
-        }).catch(err => {
-            console.log(err);
-        });
+      
 
     });
 });
